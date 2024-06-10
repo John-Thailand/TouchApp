00ee91186285bada25bc98768f05e8b2b7c89f51
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-10 17:22:21 +0900

    FrameLayoutを使ってみる

--- app/src/main/java/to/msn/wings/hello/MainActivity.kt
+++ app/src/main/java/to/msn/wings/hello/MainActivity.kt
@@ -10,22 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // 1) idを取得
-        var tx: TextView = findViewById(R.id.tv)
-        var btn1: Button = findViewById(R.id.btnDog)
-        var btn2: Button = findViewById(R.id.btnCat)
-        var btn3: Button = findViewById(R.id.btnClear)
-
-        // 2) クリック処理
-        btn1.setOnClickListener {
-            tx.text = "いぬ"
-        }
-        btn2.setOnClickListener {
-            tx.text = "ねこ"
-        }
-        btn3.setOnClickListener {
-            tx.text = "..."
-        }
     }
 }
